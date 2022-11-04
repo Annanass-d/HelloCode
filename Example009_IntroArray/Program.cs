4ee9c46fc543f9c445a1ea9ce117cabbf4f8b823
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 04:13:14 +0300

    add Example009_IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти максимальное из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {14,52,34,48,13475,64,72,28,59};
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(max);
