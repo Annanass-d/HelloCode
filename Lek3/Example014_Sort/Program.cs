709d511fbd1988cf2f957d9e811f200976f8cfbc
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-08 03:45:36 +0300

    add Example014_Sort

---
+++ Lek3/Example014_Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿// Упорядочивание элементов массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;   
+        for (int j = i +1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;     
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
