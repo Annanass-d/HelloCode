41709b4b4f2ec1090a581be3eb649f74a51bd1d0
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-16 19:41:06 +0300

    add Example015_Twodimens_Array

---
+++ Lek4/Example015_Twodimens_Array/Program.cs
@@ -0,0 +1,40 @@
+﻿// Двумерные массивы
+
+// string[,] table = new string[2, 5];
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows,columns]}-");
+//     }
+// }
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+System.Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
