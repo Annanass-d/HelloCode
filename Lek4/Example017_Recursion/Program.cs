94452ea9b021f61752b9916e101771565e303671
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-16 20:56:44 +0300

    add Example017_Recursion

---
+++ Lek4/Example017_Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿// Примеры рекурсии
+
+// int Factorial(int n)
+// {
+//     if (n == 0) return 1;
+//     else return n * Factorial(n -1);
+// }
+
+// System.Console.WriteLine(Factorial(3));
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci (int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
